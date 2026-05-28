979f19dca8d842f1414748b06c3997bdda4182a9
Benjamin Burton <benjamin.alexander.burton@gmail.com>
2026-05-28 13:40:18 +0200

    Add homepage modal for today's menus

--- mealio.client/src/pages/Home.tsx
+++ mealio.client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import ActivityCard from "../components/ActivityCard";
 import MapButton from "../components/MapButton";
+import TodayMenuList from "../components/TodayMenuList";
 import { restaurants } from "../data/restaurants";
 
 export default function Home() {
@@ -11,6 +12,8 @@ export default function Home() {
       <MapButton restaurants={restaurants} />
 
       <main className="px-5 pt-6">
+        <TodayMenuList restaurants={restaurants} />
+
         <h2 className="mb-6 text-xl font-extrabold uppercase italic text-[#0B5A4A]">
           I NÄRHETEN:
         </h2>
